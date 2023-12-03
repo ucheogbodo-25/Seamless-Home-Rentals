2bf6a26ec508caa3d11555c294cb476348aef489
ucheogbodo-25 <125726636+ucheogbodo-25@users.noreply.github.com>
2023-12-03 21:34:22 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Apartment Management System</title>
+  <title>Seamless Home Rentals</title>
   
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
